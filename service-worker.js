772904ca672a58179f84b9c39a6e016f13e09783
Savo Kordes <savo.kordes@gmail.com>
2025-08-02 12:40:50 +0200

    Add offline version of Daily Routine Tracker

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,37 @@
+const CACHE_NAME = "routine-tracker-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+];
+
+// Install service worker and cache files
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("routine-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html"]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Activate service worker and clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Intercept fetch requests
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
